2c9e5ca6db231d3075208ffcce689a547e9cbbcb
Luciano Otoni Milen <luciano.milen@concert.com.br>
2018-04-09 10:54:27 -0300

    comentários no código

--- grafo.h
+++ grafo.h
@@ -1,15 +1,23 @@
 #ifndef GRAFO
 #define GRAFO
 
-typedef struct node
+typedef struct node //estrutura recursiva de lista encadeada
 {
     struct node *prox;
-    int vertice;
+    int vertice; //identifica o valor do próximo nó
 } node;
+
+int *leIdades(int nVertices);
 
 node **leGrafo(int nVertices, int nArestas);
 
 void insere(int vi, int vj, node **Grafo);
+
+void DFS(node **Grafo, int nodeAtual, int *nGostaram, int *vNodeVisitados, int *vIdades);
+
+int verificaPrimeiro(int *vIdades, int nodeInicial);
+
+void liberaGrafo(node **Grafo, int nVertices);
 
 void imprimeGrafo(node **Grafo, int j);
 
--- main.c
+++ main.c
@@ -3,63 +3,20 @@
 #include <string.h>
 #include "grafo.h"
 
-int *leIdades(int nVertices){
-    int *vIdades = (int *) malloc((nVertices+1) * sizeof(int));
-    int lixo, i;
-    for(i = 1;i <= nVertices;i++)
-    {
-        scanf("%d %d", &lixo, &vIdades[i]);
-    }
-    return vIdades;
-}
-
-int verificaPrimeiro(int *vIdades, int nodeInicial){
-    int primeiroGostou = (vIdades[nodeInicial] < 35) ? 1 : 0;
-    return primeiroGostou;
-}
-
-void dfs(node **Grafo, int nodeAtual, int *nGostaram, int *vNodeVisitados, int *vIdades){
-    node *p = Grafo[nodeAtual];
-    vNodeVisitados[nodeAtual] = 1;
-    while (p != NULL)
-    {
-        nodeAtual = p->vertice; // age[i-1]
-        if (!vNodeVisitados[nodeAtual] && vIdades[nodeAtual] < 35){
-            *nGostaram += 1;
-            dfs(Grafo, nodeAtual, nGostaram, vNodeVisitados, vIdades);
-        }
-        p = p->prox;
-    }
-}
-
-void liberaGrafo(node **Grafo, int nVertices){
-    int i;
-    for (i = 0; i <= nVertices; i++)
-    {
-        node *p = Grafo[i];
-        while(p != NULL){
-            p = p->prox;
-        }
-        free(p);
-    }
-    free(Grafo);
-}
-
 int main(int argc, char *argv[]){
     int nodeInicial, nVertices, nArestas;
     scanf("%d %d", &nVertices, &nArestas);
-    int *vIdades = leIdades(nVertices);
-    node **Grafo = leGrafo(nVertices, nArestas);
-    //imprimeGrafo(Grafo);
+    int *vIdades = leIdades(nVertices); // lê entrada de idades dos usuários
+    node **Grafo = leGrafo(nVertices, nArestas); //inicializa o grafo com a memória necessária a partir de nVertices
     scanf("%d", &nodeInicial);
     int nGostaram = 0;
-    int *vNodeVisitados = (int*) malloc(nVertices * sizeof(int));
-    memset(vNodeVisitados, 0, (size_t) nVertices);
-    dfs(Grafo, nodeInicial, &nGostaram, vNodeVisitados, vIdades);
-    nGostaram += verificaPrimeiro(vIdades, nodeInicial);
-    liberaGrafo(Grafo, nVertices);
+    int *vNodeVisitados = (int*) malloc(nVertices * sizeof(int)); //vetor para armazenar nós visitados pelo DFS
+    memset(vNodeVisitados, 0, (size_t) nVertices); //inicializa o vetor de nós visitados como 0
+    DFS(Grafo, nodeInicial, &nGostaram, vNodeVisitados, vIdades); //faz busca em profundidade no grafo
+    nGostaram += verificaPrimeiro(vIdades, nodeInicial); //devemos contabilizar também o nó inicial se idade < 35
+    liberaGrafo(Grafo, nVertices); //libera memória alocada pro grafo
     free(vNodeVisitados);
     free(vIdades);
-    printf("%d", nGostaram);
+    printf("%d", nGostaram); // imprime resultado final
     return 0;
 }
